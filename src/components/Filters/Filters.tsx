9d3123ff8b712232b416fd50e389ffde8217e81d
Danil Chugaev <dmchugaev@gmail.com>
2026-02-05 23:45:36 +0700

    upd some styles

---
+++ src/components/Filters/Filters.tsx
@@ -0,0 +1,7 @@
+import './filters.pcss';
+
+export function Filters() {
+  return (
+    <div className="filters">filters</div>
+  );
+}
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -3,12 +3,13 @@ import { Button } from '../Button/Button.tsx';
 import { ICON_SIZE } from '../../constants.ts';
 import { PlusIcon } from '../Icon/PlusIcon.tsx';
 import { TodoItem } from './TodoItem/TodoItem.tsx';
+import { Filters } from '../Filters/Filters.tsx';
 
 export function TodoList({ items, onAddItem }: { items: any[], onAddItem: () => void }) {
   return (
     <div className="todo-list">
       <div className="todo-list__content">
-        <div className="todo-list__filters">filters</div>
+        <Filters/>
 
         <ul className="todo-list__items">
           {items.map((item: any) => (
